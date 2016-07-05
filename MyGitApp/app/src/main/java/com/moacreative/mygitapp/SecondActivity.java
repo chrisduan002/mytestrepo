0cbb074d998b86d52c5a3f4174ae4958dac531ee
chris duan <chris.duan@moacreative.com>
2016-07-05 16:21:12 +1200

    Add new files

---
+++ MyGitApp/app/src/main/java/com/moacreative/mygitapp/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.moacreative.mygitapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by chrisduan on 5/07/16.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_view);
+    }
+}
